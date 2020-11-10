efcd2681a4875725367d72fcb9b600a6b172d78d
stLmpp <gui.stlmpp@hotmail.com>
2020-11-09 21:09:48 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bio-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'biomercs-v2';
+}
