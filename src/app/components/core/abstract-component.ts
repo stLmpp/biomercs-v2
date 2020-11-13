272bc1d6f1493eecb8fd71dd506893a62df89b5f
stLmpp <gui.stlmpp@hotmail.com>
2020-11-12 23:11:44 -0300

    - added abstract component

---
+++ src/app/components/core/abstract-component.ts
@@ -0,0 +1,91 @@
+import { Directive, HostBinding, Input } from '@angular/core';
+import { BioSizeInput, BioTypeInput } from './types';
+import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
+
+@Directive()
+export abstract class AbstractComponent {
+  static ngAcceptInputType_disabled: BooleanInput;
+
+  @Input() bioType: BioTypeInput;
+
+  @HostBinding('class.primary') get primaryClass(): boolean {
+    return (this.bioType || 'primary') === 'primary';
+  }
+
+  @HostBinding('class.accent') get accentClass(): boolean {
+    return this.bioType === 'accent';
+  }
+
+  @HostBinding('class.danger') get dangerClass(): boolean {
+    return this.bioType === 'danger';
+  }
+
+  @Input() set primary(primary: BooleanInput) {
+    if (coerceBooleanProperty(primary)) {
+      this.bioType = 'primary';
+    }
+  }
+
+  @Input() set accent(accent: BooleanInput) {
+    if (coerceBooleanProperty(accent)) {
+      this.bioType = 'accent';
+    }
+  }
+
+  @Input() set danger(danger: BooleanInput) {
+    if (coerceBooleanProperty(danger)) {
+      this.bioType = 'danger';
+    }
+  }
+
+  @Input()
+  get disabled(): boolean {
+    return this._disabled;
+  }
+  set disabled(disabled: boolean) {
+    this._disabled = coerceBooleanProperty(disabled);
+  }
+  private _disabled = false;
+
+  @HostBinding('class.disabled')
+  @HostBinding('attr.disabled')
+  get disabledClass(): boolean | null {
+    return this._disabled || null;
+  }
+
+  @HostBinding('attr.aria-disabled') get ariaDisabled(): string {
+    return '' + this._disabled;
+  }
+
+  @Input() bioSize: BioSizeInput;
+
+  @HostBinding('class.small') get smallClass(): boolean {
+    return this.bioSize === 'small';
+  }
+
+  @HostBinding('class.medium') get mediumClass(): boolean {
+    return this.bioSize === 'medium';
+  }
+
+  @HostBinding('class.large') get largeClass(): boolean {
+    return this.bioSize === 'large';
+  }
+
+  @Input() set small(small: BooleanInput) {
+    if (coerceBooleanProperty(small)) {
+      this.bioSize = 'small';
+    }
+  }
+
+  @Input() set medium(medium: BooleanInput) {
+    if (coerceBooleanProperty(medium)) {
+      this.bioSize = 'medium';
+    }
+  }
+
+  @Input() set large(large: BooleanInput) {
+    if (coerceBooleanProperty(large)) {
+      this.bioSize = 'large';
+    }
+  }
+}
---
+++ src/app/components/core/types.ts
@@ -0,0 +1,4 @@
+export type BioType = 'primary' | 'accent' | 'danger';
+export type BioTypeInput = BioType | null | undefined | '';
+export type BioSize = 'small' | 'medium' | 'large';
+export type BioSizeInput = BioSize | null | undefined | '';
