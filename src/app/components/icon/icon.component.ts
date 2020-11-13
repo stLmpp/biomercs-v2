d18aaeb7ad3f6c7e632ca8885045c5b1fd89d0ff
stLmpp <gui.stlmpp@hotmail.com>
2020-11-12 23:11:55 -0300

    - added icon component

---
+++ src/app/components/icon/icon.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { AbstractComponent } from '../core/abstract-component';
+
+@Component({
+  selector: 'icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'material-icons' },
+})
+export class IconComponent extends AbstractComponent implements OnInit {
+  ngOnInit(): void {}
+}
