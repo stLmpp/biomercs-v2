e308d8c0dfaa3490e77d00aeb28e31b120920fcc
stLmpp <gui.stlmpp@hotmail.com>
2020-12-11 07:11:45 -0300

    fix(dialog): load dialog module when trying to open the dialog

--- src/app/shared/components/modal/dialog/dialog.service.ts
+++ src/app/shared/components/modal/dialog/dialog.service.ts
@@ -4,12 +4,14 @@ import { ModalRef } from '../modal-ref';
 import { DialogComponent, DialogData } from './dialog.component';
 import { from, Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
+import { DynamicLoaderService } from '../../../../core/dynamic-loader.service';
 
 @Injectable({ providedIn: 'root' })
 export class DialogService {
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private dynamicLoaderService: DynamicLoaderService) {}
 
   private async _getModalRef(data: DialogData): Promise<ModalRef<DialogComponent, DialogData, boolean>> {
+    await this.dynamicLoaderService.loadModule(() => import('./dialog.module').then(m => m.DialogModule));
     return this.modalService.openLazy(() => import('./dialog.component').then(c => c.DialogComponent), {
       panelClass: 'dialog',
       data: { btnYes: 'Yes', btnNo: 'No', ...data },
